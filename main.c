eafa082e51c62391701cb9f2ce87182463e6ccb5
Mike Turvey <mturvey6@yahoo.com>
2017-01-11 00:38:58 -0700

    Testing sample data

--- main.c
+++ main.c
@@ -165,22 +165,23 @@ void updateHeader(FILE * file)
 }
 void writeAxes(FILE * file)
 {
-    for (double i = 0; i < 1000; i++)
+    double scale = 200;
+    for (double i = 0; i < scale; i = i + scale / 1000)
     {
-        writePoint(file, i / 10, 0, 0, 255);
+        writePoint(file, i, 0, 0, 255);
     }
-    for (double i = 0; i < 1000; i++)
+    for (double i = 0; i < scale; i = i + scale/1000)
     {
-        if ((int)(i / 50) % 2 == 1)
+        if ((int)(i / (scale/5)) % 2 == 1)
         {
-            writePoint(file, 0, i / 10, 0, 255<<8);
+            writePoint(file, 0, i, 0, 255<<8);
         }
     }
-    for (double i = 0; i < 1000; i++)
+    for (double i = 0; i < scale; i = i + scale / 10001)
     {
-        if ((int)(i / 100) % 2 == 1)
+        if ((int)(i / (scale/10)) % 2 == 1)
         {
-            writePoint(file, 0, 0, i / 10, 255<<16);
+            writePoint(file, 0, 0, i, 255<<16);
         }
     }
 }
@@ -229,7 +230,7 @@ void writePointCloud(FILE *f, Point *pointCloud, unsigned int Color)
 void markPointWithStar(FILE *file, Point point, unsigned int color)
 {
     float i;
-    for ( i = -5; i <= 5; i=i +0.2)
+    for ( i = -0.8; i <= 0.8; i=i +0.0025)
     {
         writePoint(file, point.x + i, point.y, point.z, color);
         writePoint(file, point.x, point.y + i, point.z, color);
@@ -323,7 +324,7 @@ double pythAngleBetweenSensors2(TrackedSensor *a, TrackedSensor *b)
 Point SolveForLighthouse(TrackedObject *obj)
 {
     PointsAndAngle pna[MAX_POINT_PAIRS];
-    Point lh = { 100, 20, 50 };
+    Point lh = { 10, 0, 200 };
 
     size_t pnaCount = 0;
     // TODO: Need a better way of picking the pairs to use that more does a better job
@@ -351,7 +352,8 @@ Point SolveForLighthouse(TrackedObject *obj)
         }
     }
 
-    Point **pointCloud = malloc(sizeof(Point*)* pnaCount);
+    //Point **pointCloud = malloc(sizeof(Point*)* pnaCount);
+    Point **pointCloud = malloc(sizeof(void*)* pnaCount);
 
     FILE *f = fopen("pointcloud2.pcd", "wb");
     writePcdHeader(f);
@@ -394,6 +396,7 @@ Point SolveForLighthouse(TrackedObject *obj)
 
     fclose(f);
 
+    return bestMatchA;
 }
 
 static Point makeUnitPoint(Point *p)
@@ -434,45 +437,245 @@ static Point PointSub(Point a, Point b)
     return newPoint;
 }
 
-int main()
+void testRealData()
 {
 
+    TrackedObject *to;
+
+    to = malloc(sizeof(TrackedObject)+14 * sizeof(TrackedSensor));
+
+    to->numSensors = 14;
+
+    //0	190648.3331	158215.2782
+    to->sensor[0].point.x = 0.0851814;
+    to->sensor[0].point.y = 0.0170621;
+    to->sensor[0].point.z = 0.0464036;
+    to->sensor[0].theta = 190648.3331;
+    to->sensor[0].phi = 158215.2782;
+
+    //4	190442.1726	160237.0121
+    to->sensor[1].point.x = 0.0799671;
+    to->sensor[1].point.y = 0.0452252;
+    to->sensor[1].point.z = 0.0347871;
+    to->sensor[1].theta = 190442.1726;
+    to->sensor[1].phi = 160237.0121;
+        
+    //6	194906.0781	159445.4121
+    to->sensor[2].point.x = 0.0243163;
+    to->sensor[2].point.y = 0.0200039;
+    to->sensor[2].point.z = 0.0594331;
+    to->sensor[2].theta = 194906.0781;
+    to->sensor[2].phi = 159445.4121;
+
+    //7	193054.9208	159914.2366
+    to->sensor[3].point.x = 0.047366;
+    to->sensor[3].point.y = 0.0335892;
+    to->sensor[3].point.z = 0.0535793;
+    to->sensor[3].theta = 193054.9208;
+    to->sensor[3].phi = 159914.2366;
+
+    //8	194056.9867	155690.5243
+    to->sensor[4].point.x = 0.0477814;
+    to->sensor[4].point.y = -0.0340002;
+    to->sensor[4].point.z = 0.0534839;
+    to->sensor[4].theta = 194056.9867;
+    to->sensor[4].phi = 155690.5243;
+
+    //9	192905	157564.8921
+    to->sensor[5].point.x = 0.0579574;
+    to->sensor[5].point.y = -3.65E-05;
+    to->sensor[5].point.z = 0.056517;
+    to->sensor[5].theta = 192905;
+    to->sensor[5].phi = 157564.8921;
+
+    //15	191133.3968	156099.2932
+    to->sensor[6].point.x = 0.0852848;
+    to->sensor[6].point.y = -0.0171755;
+    to->sensor[6].point.z = 0.0464536;
+    to->sensor[6].theta = 191133.3968;
+    to->sensor[6].phi = 156099.2932;
+
+    //16	199287.4782	161832.5252
+    to->sensor[7].point.x = -0.047897;
+    to->sensor[7].point.y = 0.0336478;
+    to->sensor[7].point.z = 0.053597;
+    to->sensor[7].theta = 199287.4782;
+    to->sensor[7].phi = 161832.5252;
+
+    //17	198090.2071	160430.2143
+    to->sensor[8].point.x = -0.0245155;
+    to->sensor[8].point.y = 0.0200543;
+    to->sensor[8].point.z = 0.0593949;
+    to->sensor[8].theta = 198090.2071;
+    to->sensor[8].phi = 160430.2143;
+
+    //23	201748.4484	161651.8288
+    to->sensor[9].point.x = -0.0853892;
+    to->sensor[9].point.y = 0.0173502;
+    to->sensor[9].point.z = 0.0463133;
+    to->sensor[9].theta = 201748.4484;
+    to->sensor[9].phi = 161651.8288;
+
+    //24	202248.2187	159553.2756
+    to->sensor[10].point.x = -0.0852688;
+    to->sensor[10].point.y = -0.0171002;
+    to->sensor[10].point.z = 0.0462514;
+    to->sensor[10].theta = 202248.2187;
+    to->sensor[10].phi = 159553.2756;
+
+    //29	199112.6078	156248.6293
+    to->sensor[11].point.x = -0.0272585;
+    to->sensor[11].point.y = -0.0516152;
+    to->sensor[11].point.z = 0.0468868;
+    to->sensor[11].theta = 199112.6078;
+    to->sensor[11].phi = 156248.6293;
+
+    //30	200482.5997	159912.9083
+    to->sensor[12].point.x = -0.0580757;
+    to->sensor[12].point.y = 6.80E-06;
+    to->sensor[12].point.z = 0.0565004;
+    to->sensor[12].theta = 200482.5997;
+    to->sensor[12].phi = 159912.9083;
+
+    //31	200340.6642	157639.705
+    to->sensor[13].point.x = -0.0475571;
+    to->sensor[13].point.y = -0.0339427;
+    to->sensor[13].point.z = 0.0535213;
+    to->sensor[13].theta = 200340.6642;
+    to->sensor[13].phi = 157639.705;
+
+	
+
+    for (int i = 0; i < 14; i++)
+    {
+        // convert from clock cycles to degrees
+        to->sensor[i].theta = to->sensor[i].theta / 48000000 * 60 * M_PI * 2;
+        to->sensor[i].phi = to->sensor[i].phi / 48000000 * 60 * M_PI * 2;
+    }
+
+    Point foundLh = SolveForLighthouse(to);
+
+}
+
+int main()
+{
+    //testRealData();
+
     //Point lh = { 0, 0, 0 };
-    Point lh = { 100, 20, 50 };
+    Point lh = { 0, 200, 10 };
 
     TrackedObject *to;
 
     to = malloc(sizeof(TrackedObject)+4 * sizeof(TrackedSensor));
 
-    to->numSensors = 4;
-    to->sensor[0].point.x = 5.0;
-    to->sensor[0].point.y = 0.00001;
-    to->sensor[0].point.z = 0.00001;
+    to->numSensors = 6;
+    to->sensor[0].point.x = 18.0;
+    to->sensor[0].point.y = 4.0;
+    to->sensor[0].point.z = 3.0;
 
     to->sensor[1].point.x = 0.00001;
-    to->sensor[1].point.y = 5.00001;
+    to->sensor[1].point.y = 0.00001;
     to->sensor[1].point.z = 0.00001;
 
-    to->sensor[2].point.x = 0.00001;
-    to->sensor[2].point.y = 0.00001;
-    to->sensor[2].point.z = 5.00001;
+    to->sensor[2].point.x = 5.0;
+    to->sensor[2].point.y = 10.0;
+    to->sensor[2].point.z = 3.0;
 
     to->sensor[3].point.x = 0.00001;
-    to->sensor[3].point.y = 5.00001;
-    to->sensor[3].point.z = 5.00001;
+    to->sensor[3].point.y = 21.0;
+    to->sensor[3].point.z = 0.00001;
 
-    for (int i = 0; i < to->numSensors; i++)
-    {
-        double tmp = getTheta(PointSub(to->sensor[i].point, lh));
-        double tmp2 = getPhi(PointSub(to->sensor[i].point, lh));
+    to->sensor[4].point.x = 13.0;
+    to->sensor[4].point.y = 18.0;
+    to->sensor[4].point.z = 3.0;
 
-        double tmpD = tmp * 180 / M_PI;
-        double tmp2D = tmp2 * 180 / M_PI;
+    to->sensor[5].point.x = 23.0;
+    to->sensor[5].point.y = 21.0;
+    to->sensor[5].point.z = 0.00001;
 
-        to->sensor[i].theta = getTheta(PointSub(to->sensor[i].point, lh));
-        to->sensor[i].phi = getPhi(PointSub(to->sensor[i].point, lh));
-        int a = 0;
-    }
+    //for (int i = 0; i < to->numSensors; i++)
+    //{
+    //    double tmp = getTheta(PointSub(to->sensor[i].point, lh));
+    //    double tmp2 = getPhi(PointSub(to->sensor[i].point, lh));
+
+    //    double tmpD = tmp * 180 / M_PI;
+    //    double tmp2D = tmp2 * 180 / M_PI;
+
+    //    to->sensor[i].theta = getTheta(PointSub(to->sensor[i].point, lh));
+    //    to->sensor[i].phi = getPhi(PointSub(to->sensor[i].point, lh));
+    //    int a = 0;
+    //}
+#define RADIANS(x) (x / 180 * M_PI)
+
+//    0:(79.138, 91.254) 1 : (83.977, 92.572) 2 : (82.497, 89.744) 3 : (84.135, 86.627) 4 : (80.580, 87.252) 5 : (77.591, 86.291)
+    //to->sensor[0].theta = RADIANS(79.138);
+    //to->sensor[0].phi = RADIANS(91.254);
+    //to->sensor[1].theta = RADIANS(83.977);
+    //to->sensor[1].phi = RADIANS(92.572);
+    //to->sensor[2].theta = RADIANS(82.497);
+    //to->sensor[2].phi = RADIANS(89.744);
+    //to->sensor[3].theta = RADIANS(84.135);
+    //to->sensor[3].phi = RADIANS(86.627);
+    //to->sensor[4].theta = RADIANS(80.580);
+    //to->sensor[4].phi = RADIANS(87.252);
+    //to->sensor[5].theta = RADIANS(77.591);
+    //to->sensor[5].phi = RADIANS(86.291);
+
+//    0:(79.821, 87.904) 1 : (78.427, 93.046) 2 : (81.371, 91.689) 3 : (84.387, 93.179) 4 : (83.767, 89.468) 5 : (84.603, 86.709)
+    //to->sensor[0].theta = RADIANS(79.821);
+    //to->sensor[0].phi = RADIANS(87.904);
+    //to->sensor[1].theta = RADIANS(78.427);
+    //to->sensor[1].phi = RADIANS(93.046);
+    //to->sensor[2].theta = RADIANS(81.371);
+    //to->sensor[2].phi = RADIANS(91.689);
+    //to->sensor[3].theta = RADIANS(84.387);
+    //to->sensor[3].phi = RADIANS(93.179);
+    //to->sensor[4].theta = RADIANS(83.767);
+    //to->sensor[4].phi = RADIANS(89.468);
+    //to->sensor[5].theta = RADIANS(84.603);
+    //to->sensor[5].phi = RADIANS(86.709);
+
+//0:(83.614, 88.358) 1 : (78.345, 86.965) 2 : (79.828, 89.899) 3 : (78.185, 93.070) 4 : (80.894, 91.510) 5 : (84.688, 93.237)
+    //to->sensor[0].theta = RADIANS(83.614);
+    //to->sensor[0].phi = RADIANS(88.358);
+    //to->sensor[1].theta = RADIANS(78.345);
+    //to->sensor[1].phi = RADIANS(86.965);
+    //to->sensor[2].theta = RADIANS(79.828);
+    //to->sensor[2].phi = RADIANS(89.899);
+    //to->sensor[3].theta = RADIANS(78.185);
+    //to->sensor[3].phi = RADIANS(93.070);
+    //to->sensor[4].theta = RADIANS(80.894);
+    //to->sensor[4].phi = RADIANS(91.510);
+    //to->sensor[5].theta = RADIANS(84.688);
+    //to->sensor[5].phi = RADIANS(93.237);
+
+//0:(88.350, 135.574) 1 : (95.278, 136.934) 2 : (93.468, 134.097) 3 : (95.171, 131.030) 4 : (90.511, 131.695) 5 : (86.909, 130.729)
+    //to->sensor[0].theta = RADIANS(88.350);
+    //to->sensor[0].phi = RADIANS(135.574);
+    //to->sensor[1].theta = RADIANS(95.278);
+    //to->sensor[1].phi = RADIANS(136.934);
+    //to->sensor[2].theta = RADIANS(93.468);
+    //to->sensor[2].phi = RADIANS(134.097);
+    //to->sensor[3].theta = RADIANS(95.171);
+    //to->sensor[3].phi = RADIANS(131.030);
+    //to->sensor[4].theta = RADIANS(90.511);
+    //to->sensor[4].phi = RADIANS(131.695);
+    //to->sensor[5].theta = RADIANS(86.909);
+    //to->sensor[5].phi = RADIANS(130.729);
+
+    //0:(84.606, 90.598) 1 : (87.230, 91.840) 2 : (87.040, 89.078) 3 : (87.361, 86.069) 4 : (85.603, 86.617) 5 : (82.989, 85.470)
+    to->sensor[0].theta = RADIANS(84.606);
+    to->sensor[0].phi = RADIANS(90.598);
+    to->sensor[1].theta = RADIANS(87.230);
+    to->sensor[1].phi = RADIANS(91.840);
+    to->sensor[2].theta = RADIANS(87.040);
+    to->sensor[2].phi = RADIANS(89.078);
+    to->sensor[3].theta = RADIANS(87.361);
+    to->sensor[3].phi = RADIANS(86.069);
+    to->sensor[4].theta = RADIANS(85.603);
+    to->sensor[4].phi = RADIANS(86.617);
+    to->sensor[5].theta = RADIANS(82.989);
+    to->sensor[5].phi = RADIANS(85.470);
 
 
     Point foundLh = SolveForLighthouse(to);
